3626c964f067537fb53a14d1e787922b1e13d794
leepuu <leepuu001@naver.com>
2020-12-04 16:22:19 +0900

    Updates

---
+++ precache-manifest.943693de3595833a0d55ce883863e61c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d7e392927c3a4400223e3152400160",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4b8bb0318f5e20844c02",
+    "url": "/movie_app_2020/static/css/main.94f1ac4b.chunk.css"
+  },
+  {
+    "revision": "c30d332ea2693f4b69f4",
+    "url": "/movie_app_2020/static/js/2.9838505d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.9838505d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8bb0318f5e20844c02",
+    "url": "/movie_app_2020/static/js/main.e83fc164.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  },
+  {
+    "revision": "1fd572d4c6a15a93e424757ff9f9a367",
+    "url": "/movie_app_2020/static/media/header-bg.1fd572d4.jpg"
+  },
+  {
+    "revision": "3c0b25ab766bb75f15ad288440637d0a",
+    "url": "/movie_app_2020/static/media/search.3c0b25ab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.366a2b93b22a4d60e94aa8f04bc0a5c8.js"
+  "/movie_app_2020/precache-manifest.943693de3595833a0d55ce883863e61c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
